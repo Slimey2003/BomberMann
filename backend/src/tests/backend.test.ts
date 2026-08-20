9ee6d9c4a748b320e8e6d7ab7dc1773440af47dd
jbeahr <jbaehr@orgamax.de>
2026-08-20 15:36:29 +0200

    Add first todo Tests

---
+++ backend/src/tests/backend.test.ts
@@ -0,0 +1,51 @@
+import { expect, it, describe } from 'vitest'
+
+describe("Game", () => {
+    it.todo("config", () => {});
+    it.todo("running", () => {});
+    it.todo("ending", () => {});
+    it.todo("stats", () => {});
+    it.todo("ticks", () => {});
+})
+
+describe("GameField", () => {
+    it.todo("Wall", () => {});
+    it.todo("Breakable Wall", () => {});
+    it.todo("Timer", () => {});
+});
+
+describe("Player", () => {
+    it.todo("Movement", () => {});
+    describe("Collision", () => {
+        it.todo("Collision with Wall", () => {});
+        it.todo("Collision with BOM", () => {});
+    })
+    describe("Effects", () => {
+        it.todo("Has Effects", () => {});
+        it.todo("Can used it", () => {});
+    })
+    it.todo("Lost Life", () => {});
+    it.todo("Effects", () => {});
+    it.todo("Dead", () => {});
+});
+
+describe("BOM", () => {
+    describe("Placed", () => {
+        it.todo("Placed normal", () => {});
+        it.todo("Placed on BOM", () => {});
+    });
+    it.todo("Movement", () => {});
+    describe("Collision", () => {
+        it.todo("Collision with Wall", () => {});
+        it.todo("Collision with Player", () => {});
+        it.todo("Collision with BOM", () => {});
+    })
+    describe("Effects", () => {
+        it.todo("Has Effects", () => {});
+        it.todo("Can used it", () => {});
+    })
+    describe("Exposition", () => {
+        it.todo("PUFF after place", () => {});
+        it.todo("Chan Reaction", () => {});
+    });
+});
